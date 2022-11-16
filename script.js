3fcb590ebe72deae0970b8792bf15fc3f4daf5b7
edurnehugo <edurnehugo@gmail.com>
2022-11-17 00:11:10 +0100

    retoque del codigo e intentar meter imagenes de has perido o has ganado

--- script.js
+++ script.js
@@ -17,55 +17,79 @@ aciertos.dataset.count = 0;
 fallos.dataset.count = 0; 
 aciertos.textContent = `Aciertos: ${aciertos.dataset.count}`;
 fallos.textContent = `Fallos: ${fallos.dataset.count}`;
-console.log(aciertos.dataset.count);
-console.log(fallos.dataset.count);
 
 
 function generadorColores() {
   
   let coloresAleatorios = [];
   
-  let r;
+ /*  let r;
   let g;
   let b;
   
   for (let i = 0; i < 6 ; i++) {
     
-        r = Math.round(Math.random() * (256 - 0) + 0);
-        g = Math.round(Math.random() * (256 - 0) + 0);
-        b = Math.round(Math.random() * (256 - 0) + 0);
-        
-        coloresAleatorios.push(`rgb(${r}, ${g}, ${b})`);
-      }
-      
-      console.log(coloresAleatorios);
-      
-      color0.style.backgroundColor = coloresAleatorios[0];
-      color1.style.backgroundColor = coloresAleatorios[1];
-      color2.style.backgroundColor = coloresAleatorios[2];
-      color3.style.backgroundColor = coloresAleatorios[3];
-      color4.style.backgroundColor = coloresAleatorios[4];
-      color5.style.backgroundColor = coloresAleatorios[5];
-      
-      rgb = rgbRandom.textContent = `${coloresAleatorios[Math.round(Math.random() * (5 - 0) + 0)]}`.toUpperCase();
-      
-    }
-    generadorColores();
+    r = Math.round(Math.random() * (256 - 0) + 0);
+    g = Math.round(Math.random() * (256 - 0) + 0);
+    b = Math.round(Math.random() * (256 - 0) + 0);
     
-    let divs = document.querySelectorAll("div");
+    coloresAleatorios.push(`rgb(${r}, ${g}, ${b})`);
+  } */
+  let r = Math.round(Math.random() * (256 - 0) + 0);
+  let g = Math.round(Math.random() * (256 - 0) + 0);
+  let b = Math.round(Math.random() * (256 - 0) + 0);
+
+  let rNum = 0;
+  let gNum = 0;
+  let bNum = 0;
+
+  for (let i = 0; i < 6; i++) {
+    coloresAleatorios.push(`rgb(${r + rNum}, ${g + gNum}, ${b + bNum})`);
+
+    rNum += 8;
+    gNum += 9;
+    bNum += 10;
+  }
+
+
+  console.log(coloresAleatorios);
+      
+  color0.style.backgroundColor = coloresAleatorios[0];
+  color1.style.backgroundColor = coloresAleatorios[1];
+  color2.style.backgroundColor = coloresAleatorios[2];
+  color3.style.backgroundColor = coloresAleatorios[3];
+  color4.style.backgroundColor = coloresAleatorios[4];
+  color5.style.backgroundColor = coloresAleatorios[5];
+  
+  rgb = rgbRandom.textContent = `${coloresAleatorios[Math.round(Math.random() * (5 - 0) + 0)]}`.toUpperCase();
+  console.log (rgb);
+  rgbRandom.style.backgroundColor = rgb;    
+}
+
+rgbRandom.style.color = "rgb(238, 235, 235)";
+
+generadorColores();
+
+let gif = document.querySelector("#gif");
+
+let divs = document.querySelectorAll("div");
+
+function handleDivClick(e) {
+  let div = e.target;
+  if (aciertos.dataset.count < 2  && fallos.dataset.count < 2 ) {
     
-    function handleDivClick(e) {
-      let div = e.target;
-      if (aciertos.dataset.count < 2 /* || !aciertos.dataset.count) */ && fallos.dataset.count < 2 ) {
-        
-        switch (div.style.backgroundColor === rgb.toLowerCase()) {
-          case true:
-        !aciertos.dataset.count? aciertos.dataset.count = 1 : aciertos.dataset.count++
+    switch (div.style.backgroundColor === rgbRandom.style.backgroundColor) {
+      case true:
+        !aciertos.dataset.count
+        ? (aciertos.dataset.count = 1)
+        : aciertos.dataset.count++;
         generadorColores();
         break;
         
         case false:
-          !fallos.dataset.count? fallos.dataset.count = 1 : fallos.dataset.count++;
+          !fallos.dataset.count
+          ? (fallos.dataset.count = 1)
+          : fallos.dataset.count++;
           generadorColores();
           break;
         }
@@ -73,26 +97,37 @@ function generadorColores() {
         aciertos.textContent = `Aciertos: ${aciertos.dataset.count}`;
         fallos.textContent =  `Fallos: ${fallos.dataset.count}`;
         
-        console.log(aciertos.dataset.count);
-        console.log(fallos.dataset.count);
-
-      } else if (aciertos.dataset.count == 2) {
+        console.log(div);
+  } else if (aciertos.dataset.count == 2) {
 
     rgbRandom.textContent = `¡HAS GANADO!`;
     aciertos.textContent = `Aciertos: ${aciertos.dataset.count = 3}`;
-    
+
+
   } else if (fallos.dataset.count == 2) {
 
     rgbRandom.textContent = `¡HAS PERDIDO!`;
     fallos.textContent = `Fallos: ${fallos.dataset.count = 3}`;
 
+    
+   /*  const myImage = new Image(475, 356);
+    myImage.src = "snow-white-9.gif";
+    gif.append(myImage); */
+
+
+
+
   }
 
 }
 
+
 for (const div of divs) {
   div.addEventListener("click", handleDivClick);
+  
+
 }    
+
 
 let button = document.querySelector("button");
 function buttonClickHandler() {
@@ -100,7 +135,3 @@ function buttonClickHandler() {
 }
 
 button.addEventListener("click", buttonClickHandler);
-
-
-
-
