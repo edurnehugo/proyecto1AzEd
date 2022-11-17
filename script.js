d91378ba1afb2a5b23a43bac7e2f7d7f1ef1779a
edurnehugo <edurnehugo@gmail.com>
2022-11-17 20:30:34 +0100

    via zomm modificaciones con Berto

--- script.js
+++ script.js
@@ -11,12 +11,43 @@ let color5 = document.querySelector("#color5");
 
 let rgb;
 
+function getContrastColor({ r, g, b }) {
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
 
+  console.log(brightness);
+
+  if (Math.abs(brightness - 255) > brightness) {
+    return `rgb(255,255,255)`;
+  } else {
+    return `rgb(0,0,0)`;
+  }
+}
+
+function obToRGB({ r, g, b }) {
+  return `rgb(${r},${g}, ${b})`;
+}
+
+function generateRandom(min = 0, max = 100) {
+  // find diff
+  let difference = max - min;
+
+  // generate random number
+  let rand = Math.random();
+
+  // multiply with difference
+  rand = Math.floor(rand * difference);
+
+  // add with min value
+  rand = rand + min;
+
+  return rand;
+}
 
 function generadorColores() {
   let coloresAleatorios = [];
 
-  /*  let r;
+  /*
+  let r;
   let g;
   let b;
   
@@ -38,26 +69,29 @@ function generadorColores() {
   let bNum = 0;
 
   for (let i = 0; i < 6; i++) {
-    coloresAleatorios.push(`rgb(${r + rNum}, ${g + gNum}, ${b + bNum})`);
+    //coloresAleatorios.push(`rgb(${r + rNum}, ${g + gNum}, ${b + bNum})`);
+    coloresAleatorios.push({ r: r + rNum, g: g + gNum, b: b + bNum });
 
-    rNum += 8;
-    gNum += 9;
-    bNum += 10;
+    rNum += generateRandom(-15, 15); // escoger numero aleatorio entre -15 y 15
+    gNum += generateRandom(-15, 15);
+    bNum += generateRandom(-15, 15);
   }
 
   console.log(coloresAleatorios);
 
-  color0.style.backgroundColor = coloresAleatorios[0];
-  color1.style.backgroundColor = coloresAleatorios[1];
-  color2.style.backgroundColor = coloresAleatorios[2];
-  color3.style.backgroundColor = coloresAleatorios[3];
-  color4.style.backgroundColor = coloresAleatorios[4];
-  color5.style.backgroundColor = coloresAleatorios[5];
+  color0.style.backgroundColor = obToRGB(coloresAleatorios[0]);
+  color1.style.backgroundColor = obToRGB(coloresAleatorios[1]);
+  color2.style.backgroundColor = obToRGB(coloresAleatorios[2]);
+  color3.style.backgroundColor = obToRGB(coloresAleatorios[3]);
+  color4.style.backgroundColor = obToRGB(coloresAleatorios[4]);
+  color5.style.backgroundColor = obToRGB(coloresAleatorios[5]);
 
-  rgb = rgbRandom.textContent = `${
-    coloresAleatorios[Math.floor(Math.random() * coloresAleatorios.length)]
-  }`.toUpperCase();
-  rgbRandom.style.backgroundColor = rgb;
+  const target =
+    coloresAleatorios[Math.floor(Math.random() * coloresAleatorios.length)];
+
+  rgbRandom.textContent = `${obToRGB(target)}`.toUpperCase();
+  rgbRandom.style.color = getContrastColor(target);
+  rgbRandom.style.backgroundColor = obToRGB(target);
 }
 
 rgbRandom.style.color = "rgb(238, 235, 235)";
@@ -68,71 +102,69 @@ let aciertos = document.querySelector("#aciertos");
 let fallos = document.querySelector("#fallos");
 
 aciertos.dataset.count = 0;
-fallos.dataset.count = 0; 
+fallos.dataset.count = 0;
 aciertos.textContent = `Aciertos: ${aciertos.dataset.count}`;
 fallos.textContent = `Fallos: ${fallos.dataset.count}`;
 
 //let gif = document.querySelector("#gif");
 
 let divs = document.querySelectorAll("div");
+let end = false;
+
+function win() {
+  rgbRandom.textContent = `¡HAS GANADO!`;
+  //aciertos.textContent = `Aciertos: ${aciertos.dataset.count = 3}`;
+  textoFinal.textContent = `Has ganado!! `;
+  textoFinal.style.color = "rgb(233, 96, 42)";
+  end = true;
+}
+
+function loose() {
+  rgbRandom.textContent = `¡HAS PERDIDO!`;
+  //fallos.textContent = `Fallos: ${fallos.dataset.count = 3}`;
+  textoFinal.innerHTML = `
+  <img src="/imagenes/snow-white-9.gif">
+  <p>Te hemos avisado y bien no has mirado. Como a los correctos no le has dado, para ti el juego ha terminado.</p>
+  `;
+  textoFinal.style.color = "rgb(233, 96, 42)";
+  end = true;
+}
 
 function handleDivClick(e) {
   let div = e.target;
-  if (aciertos.dataset.count < 2  && fallos.dataset.count < 2 ) {
-    
-    switch (div.style.backgroundColor === rgbRandom.style.backgroundColor) {
-      case true:
-        !aciertos.dataset.count
-        ? (aciertos.dataset.count = 1)
-        : aciertos.dataset.count++;
-        generadorColores();
-        break;
-        
-        case false:
-          !fallos.dataset.count
-          ? (fallos.dataset.count = 1)
-          : fallos.dataset.count++;
-          generadorColores();
-          break;
-        }
-        
-        aciertos.textContent = `Aciertos: ${aciertos.dataset.count}`;
-        fallos.textContent =  `Fallos: ${fallos.dataset.count}`;
-         
-  } else if (aciertos.dataset.count == 2) {
 
-    rgbRandom.textContent = `¡HAS GANADO!`;
-    aciertos.textContent = `Aciertos: ${aciertos.dataset.count = 3}`;
-    textoFinal.textContent = `Has ganado!! `;
-    textoFinal.style.color = "rgb(233, 96, 42)";
+  if (end) return;
 
-
-
-  } else if (fallos.dataset.count == 2) {
-
-    rgbRandom.textContent = `¡HAS PERDIDO!`;
-    fallos.textContent = `Fallos: ${fallos.dataset.count = 3}`;
-    textoFinal.textContent = `Te hemos avisado y bien no has mirado. Como a los correctos no le has dado, para ti el juego ha terminado. `;
-    textoFinal.style.color = "rgb(233, 96, 42)";
-    
-   /*  const myImage = new Image(475, 356);
-    myImage.src = "snow-white-9.gif";
-    gif.append(myImage); */
-
-
-
-
+  if (div.style.backgroundColor === rgbRandom.style.backgroundColor) {
+    !aciertos.dataset.count
+      ? (aciertos.dataset.count = 1)
+      : aciertos.dataset.count++;
+  } else {
+    !fallos.dataset.count ? (fallos.dataset.count = 1) : fallos.dataset.count++;
   }
 
+  aciertos.textContent = `Aciertos: ${aciertos.dataset.count}`;
+  fallos.textContent = `Fallos: ${fallos.dataset.count}`;
+
+  if (aciertos.dataset.count === "3") {
+    win();
+    return;
+  }
+
+  if (fallos.dataset.count === "3") {
+    loose();
+    return;
+  }
+
+  generadorColores();
 }
 
 for (const div of divs) {
   div.addEventListener("click", handleDivClick);
-  
-}    
+}
 
 let button = document.querySelector("button");
 function buttonClickHandler() {
-    location.reload();
+  location.reload();
 }
 button.addEventListener("click", buttonClickHandler);
